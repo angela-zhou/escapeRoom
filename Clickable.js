7316e4cd2625f7a05d5b3d1e0ae90cbeb50a6f1e
Angela Zhou <36517335+angela-zhou@users.noreply.github.com>
2020-07-24 10:43:20 -0400

    Create Clickable.js

---
+++ Clickable.js
@@ -0,0 +1,16 @@
+/* global collideRectRect, mouseX, mouseY */
+
+class Clickable {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+    
+  // returns true if the rectangle and cursor collide
+  hit(cursorW, cursorH) {
+    let hit = collideRectRect(this.x, this.y, this.w, this.h, mouseX, mouseY, cursorW, cursorH);
+    return hit;
+  }
+}
